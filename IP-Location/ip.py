553670ec24e71eb26793a79a00168fcb96dab4d0
Atiger77 <atiger77@126.com>
2016-10-26 23:59:43 +0800

    Done,request url

--- IP-Location/ip.py
+++ IP-Location/ip.py
@@ -1,2 +1,13 @@
 #!/usr/bin/env python
-#
+#coding=utf-8
+import sys
+import requests
+ip = list(sys.argv)[1]
+ak = 'your own key'
+url = "http://api.map.baidu.com/highacciploc/v1?qcip="+ip+"&qterm=pc&ak="+ak+"&coord=bd09ll&extensions=1"
+r = requests.get(url)
+a =  r.json()
+if a.has_key('content'):
+    print a['content']['formatted_address']
+else:
+    print "输入IP未能找到位置。"
