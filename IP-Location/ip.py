23c5548717faa3c552721a5baeeb354592c8b37a
Atiger77 <atiger77@126.com>
2016-10-29 12:04:04 +0800

    optimize interaction

--- IP-Location/ip.py
+++ IP-Location/ip.py
@@ -2,12 +2,22 @@
 #coding=utf-8
 import sys
 import requests
-ip = list(sys.argv)[1]
-ak = 'your own key'
-url = "http://api.map.baidu.com/highacciploc/v1?qcip="+ip+"&qterm=pc&ak="+ak+"&coord=bd09ll&extensions=1"
-r = requests.get(url)
-a =  r.json()
-if a.has_key('content'):
-    print a['content']['formatted_address']
-else:
-    print "输入IP未能找到位置。"
+
+def ip_location(ip):
+    ak = 'your own key'
+    url = "http://api.map.baidu.com/highacciploc/v1?qcip="+ip+"&qterm=pc&ak="+ak+"&coord=bd09ll&extensions=1"
+    r = requests.get(url)
+    a =  r.json()
+    if a.has_key('content'):
+        print a['content']['formatted_address']
+    else:
+        print "输入IP未能找到位置。"
+
+
+if __name__ == '__main__':
+    try:
+        ip = list(sys.argv)[1]
+        ip_location(ip)
+    except IndexError:
+        print "eg: python ip.py 1.2.3.4"
+
